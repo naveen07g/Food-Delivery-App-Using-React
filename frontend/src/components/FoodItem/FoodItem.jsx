93df2298282e29897e420d3e080a82c65ce23e86
Naveen <naveen@srivatech.com>
2025-03-13 18:16:36 -0500

    Add to Cart functionality implemented

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -1,21 +1,23 @@
-import React, { useState } from 'react'
+import React, { useContext } from 'react'
 import './FoodItem.css'
 import { assets } from '../../assets/assets'
+import { StoreContext } from '../../context/StoreContext'
 
 const FoodItem = ({id, name, price, description, image}) => {
 
-    const [itemCount, setItemCount] = useState(0)
+    const {cartItems, addToCart, removeFromCart} = useContext(StoreContext);
+
   return (
     <div className='food-item'>
       <div className="food-item-img-container">
         <img className='food-item-image' src={image} alt="Image not found" />
         {
-            !itemCount 
-            ? <img className='add' onClick={() => setItemCount(prev => prev+1)} src={assets.add_icon_white} alt='Add Icon White not available'/>
+            !cartItems[id] 
+            ? <img className='add' onClick={() => addToCart(id)} src={assets.add_icon_white} alt='Add Icon White not available'/>
             : <div className='food-item-counter'> 
-                <img onClick={() => setItemCount(prev => prev-1)} src={assets.remove_icon_red} alt="Remove icon Red not available" />
-                <p>{itemCount}</p>
-                <img onClick={() => setItemCount(prev => prev+1)} src={assets.add_icon_green} alt="Add Icon Green not available" />
+                <img onClick={() => removeFromCart(id)} src={assets.remove_icon_red} alt="Remove icon Red not available" />
+                <p>{cartItems[id]}</p>
+                <img onClick={() => addToCart(id)} src={assets.add_icon_green} alt="Add Icon Green not available" />
             </div>
         }
       </div>
