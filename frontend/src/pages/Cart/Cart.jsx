546ed7b6ec33a5f9d17ad35c36ca174af1c030e6
Naveen <naveen@srivatech.com>
2025-03-13 21:56:02 -0500

    Implemented subtotal functionality in cart page

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -3,7 +3,7 @@ import './Cart.css'
 import { StoreContext } from '../../context/StoreContext'
 
 const Cart = () => {
-    const{cartItems, food_list, removeFromCart} = useContext(StoreContext)
+    const{cartItems, food_list, removeFromCart, getTotalCartAmount} = useContext(StoreContext)
   return (
     <div className='cart'>
         <div className="cart-items">
@@ -34,6 +34,39 @@ const Cart = () => {
                     )
                 }
             })}
+        </div>
+        <div className='cart-bottom'>
+            <div className="cart-total">
+                <h2>Cart Totals</h2>
+                <div>
+                    <div className="cart-total-details">
+                        <p>Subtotal</p>
+                        <p>${getTotalCartAmount()}</p>
+                    </div>
+                    <hr />
+                    <div className="cart-total-details">
+                        <p>Delivery Fee</p>
+                        <p>${2}</p>
+                    </div>
+                    <hr />
+                    <div className="cart-total-details">
+                        <b>Total</b>
+                        <b>${getTotalCartAmount()+2}</b>
+                    </div>
+                </div>
+                <button>PROCEED TO CHECKOUT</button>
+            </div>
+            <div className="cart-promocode">
+                <div>
+                    <p>
+                        If you have a promo code, Enter it here
+                    </p>
+                    <div className='cart-promocode-input'>
+                        <input type="text" placeholder='promo code'/>
+                        <button>Submit</button>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
   )
